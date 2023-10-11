ebc269eaf276430eda2f2beb9a96058cfba75218
Víctor Mato Ramos <victormator5@gmail.com>
2023-10-11 18:31:09 +0200

    Añadidos botones y parte de los string

--- app/src/main/java/com/politecnicomalaga/codechallengeintent/MainActivity.java
+++ app/src/main/java/com/politecnicomalaga/codechallengeintent/MainActivity.java
@@ -2,13 +2,34 @@ package com.politecnicomalaga.codechallengeintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TEXTO_EXTRA = "com.example.android.codechallengeintent.extra.TEXTO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void envioDeDatos(String textoSecondActivity) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(TEXTO_EXTRA,textoSecondActivity);
+        startActivity(intent);
+    }
+
+    public void empezarPrimeraActividad(View view) {
+        envioDeDatos(String.valueOf(R.string.button_1_message));
+    }
+
+    public void empezarSegundaActividad(View view) {
+        envioDeDatos();
+    }
+
+    public void empezarTerceraActividad(View view) {
     }
 }
